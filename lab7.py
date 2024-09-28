4576f4bf2638cf65028c4e975aae616ec097b661
diana-14 castano <diana.castano-gu@uniminuto.edu.co>
2024-09-28 00:03:23 +0000

    Inicio

---
+++ lab7.py
@@ -0,0 +1,8 @@
+def class_person(name, lastname, id, age, role):
+    return{
+        "name" : name,
+        "lastname" : lastname,
+        "id" : id,
+        "age" : age,
+        "role": role
+    }
